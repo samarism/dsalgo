82f0574d52de2cdc0debe0e0850e113bd57c84de
Samarjit Uppal <uppal.samar@gmail.com>
2020-11-18 12:58:23 +0530

    Update euler_tour_tree.py

--- euler_tour_tree.py
+++ euler_tour_tree.py
@@ -12,26 +12,40 @@ class Node:
 def euler_tour_tree(node: Node) -> List[int]:
     if not node:
         return []
-    res = []
+    res1 = []
 
-    def euler_tour(n, arr):
+    def euler_tour_node_val(n, arr):
         arr.append(n.val)
         if not n.right and not n.left:
             return
         if n.left:
-            euler_tour(n.left, arr)
+            euler_tour_node_val(n.left, arr)
             arr.append(n.val)
         if n.right:
-            euler_tour(n.right, arr)
+            euler_tour_node_val(n.right, arr)
             arr.append(n.val)
-    euler_tour(node, res)
 
-    return res
+    def euler_tour_depth(n, arr, level):
+        arr.append(level)
+        if not n.right and not n.left:
+            return
+        if n.left:
+            euler_tour_depth(n.left, arr, level+1)
+            arr.append(level)
+        if n.right:
+            euler_tour_depth(n.right, arr, level+1)
+            arr.append(level)
+
+    euler_tour_node_val(node, res1)
+    res2 = []
+    euler_tour_depth(node, res2, 0)
+
+    return [res1, res2]
 
 
 if __name__ == "__main__":
     tree = Node(1, Node(2, Node(3), Node(4)))
-    assert euler_tour_tree(tree) == [1, 2, 3, 2, 4, 2, 1]
+    assert euler_tour_tree(tree) == [[1, 2, 3, 2, 4, 2, 1], [0, 1, 2, 1, 2, 1, 0]]
     print(euler_tour_tree(tree))
 
 
