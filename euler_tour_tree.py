407a82f8fc29f0a9a2da27fa8e39060aa19e64ab
Samarjit Uppal <uppal.samar@gmail.com>
2020-11-18 12:43:31 +0530

    Update euler_tour_tree.py

--- euler_tour_tree.py
+++ euler_tour_tree.py
@@ -18,15 +18,21 @@ def euler_tour_tree(node: Node) -> List[int]:
         arr.append(n.val)
         if not n.right and not n.left:
             return
-        euler_tour(n.left, arr)
-        arr.append(n.val)
-        euler_tour(n.right, arr)
-        arr.append(n.val)
+        if n.left:
+            euler_tour(n.left, arr)
+            arr.append(n.val)
+        if n.right:
+            euler_tour(n.right, arr)
+            arr.append(n.val)
     euler_tour(node, res)
 
     return res
 
 
 if __name__ == "__main__":
-    tree = Node(1, Node(2, Node(3), Node(4)), Node(5))
+    tree = Node(1, Node(2, Node(3), Node(4)))
+    assert euler_tour_tree(tree) == [1, 2, 3, 2, 4, 2, 1]
     print(euler_tour_tree(tree))
+
+
+
